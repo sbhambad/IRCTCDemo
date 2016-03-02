31a8961d64132f9c1b2e7f706ce21df66f9ce03d
Anupam Sarfare <anupamsarfare@gmail.com>
2016-03-02 18:14:00 +0530

    fare enq

--- IRCTC/app/src/main/java/com/zuneeue/irctcinfo/FareEnquiryActivity.java
+++ IRCTC/app/src/main/java/com/zuneeue/irctcinfo/FareEnquiryActivity.java
@@ -1,12 +1,20 @@
 package com.zuneeue.irctcinfo;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class FareEnquiryActivity extends AppCompatActivity {
-    Toolbar mToolbar;
+import com.zuneeue.irctcinfo.Utils.DatePickerFragment;
+
+public class FareEnquiryActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private Toolbar mToolbar;
+    private EditText date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,17 @@ public class FareEnquiryActivity extends AppCompatActivity {
         mToolbar.setTitle("Fare Enquiry");
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final DatePickerFragment newFrag = new DatePickerFragment();
+
+        date = (EditText) findViewById(R.id.date);
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newFrag.show(getSupportFragmentManager(), "Date");
+            }
+        });
 
     }
 
@@ -35,6 +54,13 @@ public class FareEnquiryActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        if (view.getTag().equals("Date")) {
+            date.setText(monthOfYear + "/" + dayOfMonth + "/" + year);
+        }
     }
 
 }
